c3c5510dc2fa193d6e4a0ef0f317fd8c319c33a2
Gustavo Martins dos Santos <gugumartin77@gmail.com>
2022-10-03 19:17:03 -0300

    feat: implement main

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.set('trust proxy', 1);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste Rise')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .addTag('Status')
+    .addTag('Auth')
+    .addTag('User')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 5005);
+}
+bootstrap();
