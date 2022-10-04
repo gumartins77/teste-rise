1d9c4c2ee55fc360bc25e65a1cb5bf26e04d26f1
Gustavo Martins dos Santos <gugumartin77@gmail.com>
2022-10-03 21:26:55 -0300

    feat: implement event entity

---
+++ src/modules/event/entities/event.entity.ts
@@ -0,0 +1,13 @@
+export class Event {
+  id?: number;
+  name: string;
+  description: string;
+  imageGallery: string;
+  eventDataTime: string;
+  eventLocation: string;
+  locationCoordinates: string;
+  importantInformation: string;
+  eventMap: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
